e89da12f030e524e6a9fc4e07a0a0a4dcfd7a49f
bleedcloudVIII <yurikaras8@gmail.com>
2025-10-22 20:25:39 +1000

    BinOperatorNode добавил поддержку новых операторов

--- compiler/ast/nodes/bin_operator.py
+++ compiler/ast/nodes/bin_operator.py
@@ -25,7 +25,19 @@ class BinOperatorNode(Node):
 
         return left - right
 
+    def _execute_miltiplication(self):
+        left, right = self._get_int_values()
+
+        return left * right
+
+    def _execute_division(self):
+        left, right = self._get_int_values()
+
+        return left / right
+
     def _validate_values(self):
+        # FIXME По идеи надо проверять, чтобы был один тип и возвращать его
+        # И уже потом менять логику расчёта для каждого типа в _execute_ методах
         is_left_value_number = self.left_value.token_type == TokenType.NUMBER
         is_right_value_number = self.right_value.token_type == TokenType.NUMBER
 
@@ -36,10 +48,15 @@ class BinOperatorNode(Node):
 
     def execute(self):
         self._validate_values()
+        token_type = self.operator.token_type
 
-        if self.operator.token_type == TokenType.PLUS:
+        if token_type == TokenType.PLUS:
             return self._execute_plus()
-        elif self.operator.token_type == TokenType.MINUS:
+        elif token_type == TokenType.MINUS:
             return self._execute_minus()
+        elif token_type == TokenType.MULTIPLICATION:
+            return self._execute_miltiplication()
+        elif token_type == TokenType.DIVISION:
+            return self._execute_division()
 
         raise Exception(f"BinOperatorNode: operator {self.operator.token_type.name} is not supported")
