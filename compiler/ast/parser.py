f37d94949f15d907c73f74898319710d164f3556
bleedcloudVIII <yurikaras8@gmail.com>
2025-11-15 18:25:24 +1000

    Парсер для линков

--- compiler/ast/parser.py
+++ compiler/ast/parser.py
@@ -1,8 +1,12 @@
+from compiler.ast.nodes.common_nodes.link_node import LINKS_OPERATORS_TOKEN_TYPE
 from compiler.common.token import Token
 from compiler.common.token_type import TokenType
 from compiler.ast.nodes.common_nodes.assign_node import AssignNode
-from compiler.ast.nodes.common_nodes.bin_operator_node import BinOperatorTokenTypes, BinOperatorNode
+from compiler.ast.nodes.common_nodes.bin_operator_node import BIN_OPERATOR_TOKEN_TYPES, BinOperatorNode
 from compiler.ast.nodes.common_nodes.number_node import NumberNode
+from compiler.ast.nodes.common_nodes.link_node import LinkNode
+from compiler.ast.nodes.common_nodes.reverse_link_node import ReverseLinkNode
+from compiler.ast.nodes.common_nodes.path_node import PathNode
 from typing import List
 
 
@@ -46,10 +50,13 @@ class Parser():
     def delta_token(self, delta: int):
         return self.tokens[self.position + delta]
 
-    def parse_word_node(self, position: int = None):
+    def parse_word_node(self, position: int = None, token: Token = None):
         from compiler.ast.nodes.common_nodes.word_node import WordNode
         if position is None:
             position = self.position
+
+        if token:
+            return WordNode(token)
 
         token = self.token(position)
         return WordNode(token)
@@ -62,7 +69,7 @@ class Parser():
         value = self.token(position + 1)
         next_token = self.token(position + 2)
 
-        if next_token.token_type in BinOperatorTokenTypes:
+        if next_token.token_type in BIN_OPERATOR_TOKEN_TYPES:
             value, delta = self.parse_bin_operator_node(position + 2)
 
         if variable.token_type == TokenType.WORD:
@@ -83,16 +90,15 @@ class Parser():
         operator = self.token(position)
 
         if position + 1 > self.length:
-            raise Exception("Parser: doesn't have all values")
+            raise Exception("Parser: doesn't have all values for BinOperatorNode")
 
-        # TODO NumberNode а не Token
         left = self.token(position - 1)
         right = self.token(position + 1)
 
-        if left.token_type in BinOperatorTokenTypes:
+        if left.token_type in BIN_OPERATOR_TOKEN_TYPES:
             left = self.parse_bin_operator_node(position - 1)
 
-        if right.token_type in BinOperatorTokenTypes:
+        if right.token_type in BIN_OPERATOR_TOKEN_TYPES:
             right = self.parse_bin_operator_node(position + 1)
 
         if left.token_type == TokenType.NUMBER:
@@ -110,6 +116,65 @@ class Parser():
             2
         )
 
+    def _parse_link(self, left: Token, right: Token):
+        # left - source
+        # right - word | [] (layer)
+
+        if left.token_type == TokenType.STRING:
+            left = PathNode(left)
+        elif left.token_type == TokenType.WORD:
+            left = self.parse_word_node(token=left)
+
+        if right.token_type == TokenType.WORD:
+            right = self.parse_word_node(token=right)
+        # TODO дописать, когда появится новая логика
+
+        return (
+            LinkNode(
+                left=left,
+                right=right
+            ),
+            2
+        )
+
+    def _parse_reverse_link(self, left: Token, right: Token):
+        # right - word | []
+        # right - source
+
+        if right.token_type == TokenType.STRING:
+            right = PathNode(right)
+        elif right.token_type == TokenType.WORD:
+            right = self.parse_word_node(token=right)
+
+        if left.token_type == TokenType.WORD:
+            left = self.parse_word_node(token=left)
+
+        return (
+            ReverseLinkNode(
+                left=left,
+                right=right
+            ),
+            2
+        )
+
+    def parse_link_operator_node(self, position: int = None):
+        if position is None:
+            position = self.position
+
+        if position + 1 > self.length:
+            raise Exception("Parser: doesn't have all values for LinkNode")
+
+        operator = self.token(position)
+        left = self.token(position - 1)
+        right = self.token(position + 1)
+
+        if operator.token_type == TokenType.LINK:
+            return self._parse_link(left, right)
+        elif operator.token_type == TokenType.REVERSE_LINK:
+            return self._parse_reverse_link(left, right)
+        else:
+            raise Exception("Parser: Unknown operator_type in _parse_link_operator_node")
+
     def _parse_line(self, line_position: int = None):
         if line_position is None:
             line_position = self.line_position
@@ -125,14 +190,19 @@ class Parser():
                 self.nodes.append(node)
                 self.position += delta
             elif self.current_token_type == TokenType.WORD:
+                # TODO Смотреть следующий токен, если бинарный или линк пропускать, иначе ошибка?
                 # self.nodes.append(self.parse_word_node())
                 self.position += 1
-            elif self.current_token_type in BinOperatorTokenTypes:
+            elif self.current_token_type in BIN_OPERATOR_TOKEN_TYPES:
                 node, delta = self.parse_bin_operator_node()
                 self.nodes.append(node)
                 self.position += delta
-            # else:
-            #     self.position += 1
+            elif self.current_token_type in LINKS_OPERATORS_TOKEN_TYPE:
+                node, delta = self.parse_link_operator_node()
+                self.nodes.append(node)
+                self.position += delta
+            else:
+                self.position += 1
 
         # return self.nodes
 
