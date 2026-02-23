4a3c79609d753a327a8198dc97711d0d27959925
bleedcloudVIII <yurikaras8@gmail.com>
2026-02-23 14:07:46 +1000

    Убрал лишнее

--- compiler/ast/nodes/common_nodes/word_node.py
+++ compiler/ast/nodes/common_nodes/word_node.py
@@ -8,36 +8,11 @@ class WordNode(Node):
         if word.token_type != TokenType.WORD:
             raise Exception("WordNode: Token type is wrong")
 
+        word.token_text.strip()
         self.token = word
-
-    @property
-    def is_key_word(self):
-        pass
-        # from compiler.common.key_words import KEY_WORDS_VALUE_NAME
-        # return self.token.token_text in KEY_WORDS_VALUE_NAME
-
-    def is_function(self):
-        return False
 
     def execute(self):
         pass
-        # if not self.is_key_word:
-        #     return VARIABLES[self.token.token_text]
-
-        # if not self.is_function():
-        #     pass
-
-        # По идеи сюда никак не попадёт
-        # raise Exception("WordNode: unrecognized word")
-
-    def set(self, new_value):
-        pass
-        # VARIABLES[self.token.token_text] = new_value
-        # return None
-
-    def get(self):
-        pass
-        # return self.execute()
 
     def __str__(self):
         return f"WordNode<{self.token.token_text}>"
